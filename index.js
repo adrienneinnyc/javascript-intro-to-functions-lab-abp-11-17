1bca11417f80fae0c7dc069b80d7d42af9fca212
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-11 17:10:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,11 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === `${string.toLowerCase()}`) {
     return "I can't hear you!";
-  }
-}
-
-function sayHiToGrandma(string) {
-  if (string === `${string.toUpperCase}`) {
+  } elseif (string === `${string.toUpperCase()}`) {
     return "YES, INDEED!";
   }
 }
+
+
